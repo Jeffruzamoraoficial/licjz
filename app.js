644481da1f67e23c69c5561332e4df10aef3a7dd
Jeffruzamoraoficial <95389530+Jeffruzamoraoficial@users.noreply.github.com>
2022-02-07 12:14:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const hamburger = document.querySelector('.header .header-nav .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .header-nav .nav-list ul');
-const menu_item = document.querySelectorAll('.header .header-nav .nav-list ul li a');
-const header = document.querySelector('.header.container');
+const  hamburguesa  =  documento . querySelector ( '.header .header-nav .nav-list .hamburger' ) ;
+const  mobile_menu  =  documento . querySelector ( '.header .header-nav .nav-list ul' ) ;
+const  menu_item  =  documento . querySelectorAll ( '.header.header-nav.nav-list ul li a' ) ;
+ encabezado  const =  documento . querySelector ( '.header.container' ) ;
 
-hamburger.addEventListener('click',  () => {
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
-});
+hamburguesa _ addEventListener ( 'clic' ,   ( )  =>  {
+    hamburguesa _ listaclases . alternar ( 'activo' ) ;
+    menú_móvil . listaclases . alternar ( 'activo' ) ;
+} ) ;
 
-document.addEventListener('scroll', () => {
-    var scroll_position = window.scrollY;
-    if( scroll_position > 100){
-        header.style.backgroundColor = "#000000";
+documento _ addEventListener ( 'desplazamiento' ,  ( )  =>  {
+    var  scroll_position  =  ventana . desplazarse Y ;
+    si (  posición_desplazamiento  >  100 ) {
+        encabezado _ estilo _ color de fondo  =  "#000000" ;
     }
-    else{
-        header.style.backgroundColor = "rgba(31, 30, 30, 0.158)";
+    más {
+        encabezado _ estilo _ colordefondo  =  "rgba(31, 30, 30, 0.158)" ;
     }
-})
-menu_item.forEach(item => {
-    item.addEventListener('click',() => {
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
-    })
-})
+} )
+menu_item . paraCada ( elemento  =>  {
+    artículo _ addEventListener ( 'clic' , ( )  =>  {
+    hamburguesa _ listaclases . alternar ( 'activo' ) ;
+    menú_móvil . listaclases . alternar ( 'activo' ) ;
+    } )
+} )
