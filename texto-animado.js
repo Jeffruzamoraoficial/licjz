035a731b745d64a9e2954ec1db7bbbbe3be3c460
Jeffruzamoraoficial <95389530+Jeffruzamoraoficial@users.noreply.github.com>
2022-03-10 23:15:59 -0600

    Update texto-animado.js

--- texto-animado.js
+++ texto-animado.js
@@ -1,5 +1,5 @@
 const typed = new Typed(".inicio-texto__aleatorio", {
-    strings: [, "Desarrollador Web", "FreeLancer", "Diseñador UI", "Experto En Maquetación"],
+    strings: [, "Desarrollador Web", "Auditor", "Contador", "Experto en Android"],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true,
