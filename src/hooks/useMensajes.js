d9d81db592627bcdf5f77f982cc34ea32a4b1be1
Jeffruzamoraoficial <95389530+Jeffruzamoraoficial@users.noreply.github.com>
2022-09-06 14:15:28 -0600

    Delete src directory

--- src/hooks/useMensajes.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import MensajesContext from "../context/MensajesProvider";
-
-const useMensajes = () => {
-  return useContext(MensajesContext);
-};
-
-export default useMensajes;
--- src/js/texto-animado.js
+++
@@ -1,6 +0,0 @@
-const typed = new Typed(".inicio-texto__aleatorio", {
-    strings: [, "Desarrollador Web", "FreeLancer", "Diseñador UI", "Experto En Maquetación"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-});
